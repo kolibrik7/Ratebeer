406503a16d8379579a28826d1e28f9690c249e39
kolibrik7 <kolibrik7@gmail.com>
2017-07-28 20:08:02 +0200

    vytvoreny beer model v databaze

--- beers/models.py
+++ beers/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class Beer(models.Model):
+	STYLE_CHOICES = (
+		("lager", "Ležiak"),
+		("ale", "Ale"),
+	)
+
+	brewery = models.CharField(max_length=50)
+	name = models.CharField(max_length=70)
+	style = models.CharField(
+		max_length=20,
+		choices="STYLE_CHOICES"
+	)
+	plato = models.IntegerField()
+	abv = models.FloatField()
+	city = models.CharField(max_length=50)
+	place = models.CharField(max_length=70)
+	note = models.TextField()
