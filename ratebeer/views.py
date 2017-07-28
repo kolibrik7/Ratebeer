6065ee99335e7f970c1d96dc57f76f31340b7eb4
kolibrik7 <kolibrik7@gmail.com>
2017-07-28 18:55:38 +0200

    v index opraveny return, druhy argument renderu

--- ratebeer/views.py
+++ ratebeer/views.py
@@ -3,7 +3,7 @@ from django.contrib.auth import login, authenticate
 from .forms import RegistrationForm
 
 def index(request):
-	return render(request, "index.html")
+	return render(request, "beers/index.html")
 
 def registration(request):
 	if request.method == "POST":
