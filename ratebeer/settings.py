e200c506b26da13ddc9f4acd9706fd029d069fee
kolibrik7 <kolibrik7@gmail.com>
2017-07-28 20:55:22 +0200

    opravena cesta k static suborom

--- ratebeer/settings.py
+++ ratebeer/settings.py
@@ -124,7 +124,8 @@ DATE_FORMAT = 'j F Y'
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATIC_ROOT = os.path.join(BASE_DIR, '../static/')
+STATICFILES_DIRS = ('/home/fred/Development/Ratebeer/static/',)
 
 LOGIN_REDIRECT_URL = '/beers/'
 LOGOUT_REDIRECT_URL = '/'
