229c57029902cb5bfcb0a1148d2f024cb83ebd96
kolibrik7 <kolibrik7@gmail.com>
2017-07-31 14:54:23 +0200

    vytvoreny formular pre Beer model

--- beers/forms.py
+++ beers/forms.py
@@ -6,6 +6,13 @@ import datetime
 class BeerForm(forms.ModelForm):
 
 	date = forms.DateField(label="Dátum", widget=forms.SelectDateWidget(), initial=datetime.date.today)
+	plato = forms.FloatField(required=False, label="Stupňovitosť", min_value=6, max_value=40,
+		widget=NumberInput(attrs={'id': 'stupnovitost', 'step': "0.5"}))
+	abv = forms.FloatField(required=False, label="Alkohol", min_value=0, max_value=20,
+		widget=NumberInput(attrs={'id': 'alkohol', 'step': "0.1"}))
+	price = forms.FloatField(required=False, label="Cena", min_value=0, max_value=5,
+		widget=NumberInput(attrs={'id': 'cena', 'step': "0.1"}))
+
 
 	class Meta:
 		model = Beer
@@ -17,13 +24,14 @@ class BeerForm(forms.ModelForm):
 			'brewery': _("Pivovar"),
 			'name': _("Názov"),
 			'style': _("Štýl"),
-			'plato': _("Stupňovitosť"),
-			'abv': _("Alkohol"),
 			'city': _("Mesto"),
 			'place': _("Podnik/Miesto"),
 			'serving': _("Formát"),
-			'price': _("Cena"),
 			'volume': _("Objem"),
 			'note': _("Poznámka"),
 			'rating': _("Hodnotenie"),
+		}
+
+		widgets = {
+			'serving': forms.RadioSelect
 		}
