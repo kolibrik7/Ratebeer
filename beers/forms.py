fd2666189a6594c33c3aff48d737c0664f54a6d8
kolibrik7 <kolibrik7@gmail.com>
2017-07-31 15:58:29 +0200

    note required false, upraveny widget na roky

--- beers/forms.py
+++ beers/forms.py
@@ -5,13 +5,19 @@ import datetime
 
 class BeerForm(forms.ModelForm):
 
-	date = forms.DateField(label="Dátum", widget=forms.SelectDateWidget(), initial=datetime.date.today)
+	def years():
+		current_year = datetime.datetime.now().year
+		first_year = current_year - 5
+		return list(range(first_year, current_year + 1))
+
+	date = forms.DateField(label="Dátum", widget=forms.SelectDateWidget(years=years()), initial=datetime.date.today)
 	plato = forms.FloatField(required=False, label="Stupňovitosť", min_value=6, max_value=40, initial=12,
 		widget=forms.NumberInput(attrs={'id': 'stupnovitost', 'step': "0.5"}))
 	abv = forms.FloatField(required=False, label="Alkohol", min_value=0, max_value=20, initial=5,
 		widget=forms.NumberInput(attrs={'id': 'alkohol', 'step': "0.1"}))
 	price = forms.FloatField(required=False, label="Cena", min_value=0, max_value=5, initial=1.5,
 		widget=forms.NumberInput(attrs={'id': 'cena', 'step': "0.1"}))
+	note = forms.CharField(required=False, label="Poznámka", widget=forms.Textarea())
 
 
 	class Meta:
@@ -29,7 +35,6 @@ class BeerForm(forms.ModelForm):
 			'place': _("Podnik/Miesto"),
 			'serving': _("Formát"),
 			'volume': _("Objem"),
-			'note': _("Poznámka"),
 			'rating': _("Hodnotenie"),
 		}
 
