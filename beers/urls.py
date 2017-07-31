e8fb77fa00a615b2638963cf4a6ecf2692a243e6
kolibrik7 <kolibrik7@gmail.com>
2017-07-31 14:59:45 +0200

    zmenene meno url na slovenske

--- beers/urls.py
+++ beers/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
 	url(r'^$', views.zoznam, name="zoznam"),
-	url(r'^add_beer/$', views.pridanie_piva, name="pridanie_piva")
+	url(r'^pridanie_piva/$', views.pridanie_piva, name="pridanie_piva")
 ]
--- beers/views.py
+++ beers/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
 
 from .models import Beer
+from .forms import BeerForm
 
 def zoznam(request):
 	return render(request, "beers/zoznam.html")
+
+def pridanie_piva(request):
+	if request.method == "POST":
+		form = BeerForm(request.POST)
+		if form.is_valid():
+			form.save()
+			return HttpResponseRedirect("/beers/")
+	else:
+		form = BeerForm()
+
+	return render(request, "beers/add_beer.html")
